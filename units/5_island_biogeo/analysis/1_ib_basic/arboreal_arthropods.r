d4b709a3604f0735eb356b7e3d7dc95a2252188d
Michael S. Taylor <mtaylor@semo.edu>
2023-10-13 04:43:51 -0500

    Code clean up.

--- units/5_island_biogeo/analysis/1_ib_basic/arboreal_arthropods.r
+++ units/5_island_biogeo/analysis/1_ib_basic/arboreal_arthropods.r
@@ -1,11 +1,26 @@
-xyplot(species~as.factor(year) | island, data = dat, aspect='xy', type='o', xlim=c(69,71)
-	panel = function(x,y,...){
-	panel.text(x,y+2,dat$year, cex=0.7)
-	panel.xyplot(x,y,...)
+dat <- read.csv("arboreal_arthropods.csv")
+
+xyplot(
+  species ~ as.factor(year) | island, 
+  data = dat, 
+  aspect = 'xy', 
+  type = 'o', 
+  xlim = c(69,71),
+	panel = function(x, y, ...){
+	panel.text(x, y + 2, dat$year, cex = 0.7)
+	panel.xyplot(x, y, ...)
 	}
 )
 
-xyplot(species~as.factor(year) | island, data = dat, aspect='xy', type='o')
+xyplot(
+  species ~ as.factor(year) | island, 
+  data = dat, 
+  aspect = "xy", 
+  type = "o")
 
-xyplot(species~area | island, data = dat, aspect='xy', type='o')
+xyplot(
+  species ~ area | island, 
+  data = dat, 
+  aspect = "xy", 
+  type = "o")
 
